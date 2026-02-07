ffb25bc1e9049a1a5c21df5c5ca211e9e6e10a7d
AADI13200 <aadeshbendre@gmail.com>
2026-02-08 00:56:32 +0530

    linear search updated

--- linear_srch.py
+++ linear_srch.py
@@ -4,7 +4,7 @@ def linear_srch(a,trget):
             print("Element found at index : ",i)
             break
 
-a=[1,2,3,4,5]
-target=int(input("Enter the element to be searched: "))
+a=[1,2,3,4,5,6,7,8,9,10]
+target=int(input("Enter the element to be searched : "))
 print("The array is:",a)
 linear_srch(a,target)

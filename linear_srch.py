885c641b79f6e194b879fa335621b9bcc58f9486
AADI13200 <aadeshbendre@gmail.com>
2026-02-08 00:54:35 +0530

    Linear search

---
+++ linear_srch.py
@@ -0,0 +1,10 @@
+def linear_srch(a,trget):
+    for i in range(len(a)):
+        if a[i]==target:
+            print("Element found at index : ",i)
+            break
+
+a=[1,2,3,4,5]
+target=int(input("Enter the element to be searched: "))
+print("The array is:",a)
+linear_srch(a,target)

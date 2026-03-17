2331d9e10e84dcc9f40c6a9fe66fdda27d2eb369
AADI13200 <aadeshbendre@gmail.com>
2026-03-17 23:47:50 +0530

    binary search lower bound

---
+++ lower_bound.py
@@ -0,0 +1,34 @@
+# brute force approach
+
+'''def lowerbound(a,x):
+    for i in range(len(a)):
+        if a[i]>=x:
+            print(i)
+            break
+
+a=[3, 5, 8, 15, 19]
+x=9
+
+y=lowerbound(a,x)'''
+
+
+# optimal approach
+
+def lowerbnd(a,y):
+    low,high=0,len(a)-1
+    ans=len(a)
+
+    while low <= high:
+        mid=(low+high)//2
+        if a[mid]>=y:
+            ans=mid
+            high=mid-1
+
+        else:
+            low=mid+1
+
+    print(ans)
+
+a=[3, 5, 8, 15, 19]
+y=9
+c=lowerbnd(a,y)
